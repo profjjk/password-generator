71e54eb2b77faa1afd81a317ba90a1b228acf41d
Jordan Kelly <jordan.j.kelly101@gmail.com>
2020-12-16 19:10:11 -0800

    Declare all variables and arrays

--- script.js
+++ script.js
@@ -15,3 +15,13 @@ generateBtn.addEventListener("click", writePassword);
 
 
 //////////////////////////////////////
+
+
+// Declare all arrays and variables.
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var alphaLower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var specChar = ["!", "#", "$", "%", "&", "?", "/", "@", "*", "(", ")", "<", ">"];
+var passwordComp = [];
+var passwordLength = 0;
+var password = "";
